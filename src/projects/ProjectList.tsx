30e2c4ea31430929dd1c713ee559330de7c3cc45
arnoldsouth <arnoldsouth@gmail.com>
2023-11-21 07:03:56 -0600

    lab 14 complete

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -8,10 +8,11 @@ import ProjectForm from './ProjectForm';
 // Create interface to define the properties that come into the component
 interface ProjectListProps {
   projects: Project[];
+  onSave: (project: Project) => void;
 }
 
 // Create a functional component that takes `projects` array as a prop
-const ProjectList = ({ projects }: ProjectListProps) => {
+const ProjectList = ({ projects, onSave }: ProjectListProps) => {
   const [projectBeingEdited, setProjectBeingEdited] = useState({});
 
   const handleEdit = (project: Project) => {
@@ -29,7 +30,7 @@ const ProjectList = ({ projects }: ProjectListProps) => {
         {projects.map((project) => (
           <div key={project.id} className="cols-sm">
             {project === projectBeingEdited ? (
-              <ProjectForm onCancel={cancelEditing} />
+              <ProjectForm onCancel={cancelEditing} onSave={onSave} />
             ) : (
               <ProjectCard project={project} onEdit={handleEdit} />
             )}
