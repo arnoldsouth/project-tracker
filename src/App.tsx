66dde57fbb7a590165cc26a5191e84e26f14dd80
arnoldsouth <arnoldsouth@gmail.com>
2023-12-02 01:32:34 -0800

    lab 20 complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,39 @@ import React, { useEffect, useState } from 'react';
 
 import './App.css';
 import ProjectsPage from './projects/ProjectsPage';
+import {
+  NavLink,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from 'react-router-dom';
+import Homepage from './home/Homepage';
 
 function App() {
   return (
-    <div className="container">
-      <ProjectsPage />
-    </div>
+    <Router>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
